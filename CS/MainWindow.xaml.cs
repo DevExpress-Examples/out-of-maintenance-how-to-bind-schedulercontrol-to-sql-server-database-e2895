4df5d8e7a42131bd9619d751c85903db71c2e145
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using DevExpress.XtraScheduler;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+
+namespace SchedulerCarsSQLServerWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private CarsDBDataSet dataSet = new CarsDBDataSet();
+        private CarsDBDataSetTableAdapters.CarSchedulingTableAdapter tableAdapterAppointments = new CarsDBDataSetTableAdapters.CarSchedulingTableAdapter();
+        private CarsDBDataSetTableAdapters.CarsTableAdapter tableAdapterResources = new CarsDBDataSetTableAdapters.CarsTableAdapter();
+        
+        public MainWindow() {
+            InitializeComponent();
+
+            schedulerControl1.Storage.Appointments.ResourceSharing = true;
+            schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Week;
+
+            tableAdapterResources.Fill(dataSet.Cars);
+            tableAdapterAppointments.Fill(dataSet.CarScheduling);
+
+            schedulerControl1.Storage.Resources.DataSource = dataSet.Cars;
+            schedulerControl1.Storage.Appointments.DataSource = dataSet.CarScheduling;
+
+            schedulerControl1.Start = schedulerControl1.Storage.Appointments[0].Start;
+
+            schedulerControl1.Storage.AppointmentsInserted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            schedulerControl1.Storage.AppointmentsChanged +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            schedulerControl1.Storage.AppointmentsDeleted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+
+            tableAdapterAppointments.Adapter.RowUpdated += 
+                new System.Data.SqlClient.SqlRowUpdatedEventHandler(Adapter_RowUpdated);
+        }
+
+        void Storage_AppointmentsModified(object sender, PersistentObjectsEventArgs e) {
+            this.tableAdapterAppointments.Adapter.Update(this.dataSet);
+            this.dataSet.AcceptChanges();
+        }
+
+        void Adapter_RowUpdated(object sender, System.Data.SqlClient.SqlRowUpdatedEventArgs e) {
+            if (e.Status == UpdateStatus.Continue && e.StatementType == StatementType.Insert) {
+                int id = 0;
+                using (SqlCommand cmd = new SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", tableAdapterAppointments.Connection)) {
+                    id = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+                e.Row["ID"] = id;
+            }
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to bind SchedulerControl to SQL Server database
+
+
+<p>This example demonstrates how to bind the SchedulerControl to a database located in MS SQL Server 2005.</p><p>The basic steps to create this example are equivalent to the steps described in <a href="http://documentation.devexpress.com/#WPF/CustomDocument8653"><u>Lesson 1 - Bind a Scheduler to Data</u></a>. However, several additional steps are added to demonstrate more SchedulerControl capabilites:</p><p>- Resources for appointments<br />
+Besides the appointment mappings, resource mappings are defined in the SchedulerStorage. A separate TableAdapter is used to retrieve resources from the database. </p><p>- Resource sharing<br />
+The <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraSchedulerAppointmentStorageBase_ResourceSharingtopic"><u>AppointmentStorageBase.ResourceSharing Property</u></a> value is set to true (note the <a href="https://www.devexpress.com/Support/Center/p/B182164">It is impossible to set ResourceSharing property to true via XAML</a> bug report).</p><p>To test this example locally, you should setup our CarsXtraScheduling sample database in your SQL Server instance (you can download the corresponding *.sql scripts from the <a href="https://www.devexpress.com/Support/Center/p/E215">How to bind ASPxScheduler to MS SQL Server database</a> example).</p>
+
+<br/>
+
+
